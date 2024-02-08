bfb5e2e4aa874dda680cb161b53347b3c046f558
Alejandro Chavez Nuñez <fullstack@naranti.com.mx>
2024-02-08 12:57:29 -0600

    Importaciones

---
+++ src/bases/08-importaciones.js
@@ -0,0 +1,26 @@
+import  heroes, {owners}  from "./data/heroes";
+import  { heroesSimple }  from "./data/heroes";
+
+const getHeroeByIdArr = (id, arr) => {
+    return arr.find((hero) => hero.id === id);
+}
+
+const getHeroeById = (id) => {
+    return heroes.find((hero) => hero.id === id);
+}
+
+const getHeroeByIdSimple = (id) => heroes.find(hero => hero.id === id);
+
+const getHeroesByOwner = (owner) => heroes.filter(hero => hero.owner === owner);
+
+const getHero = (hero) => {
+    return hero.id === 2;
+}
+
+console.log('Usando scope local', getHeroeByIdArr(2, heroes));
+console.log('Usando solo el id con el heroes global', getHeroeById(2));
+console.log('Usando un callback directamente en el global', heroes.find(getHero) );
+console.log('Usando una sentencia mas simple', getHeroeByIdSimple(3));
+console.log('Usando filter by owner MARVEL', getHeroesByOwner(owners[0]));
+console.log('Usando filter by owner DC', getHeroesByOwner(owners[1]));
+
---
+++ src/data/heroes.js
@@ -0,0 +1,65 @@
+// multiples importaciones y exportaciones
+export const heroes = [
+    {
+        id: 1,
+        name: 'Batman',
+        owner: 'DC'
+    },
+    {
+        id: 2,
+        name: 'Spiderman',
+        owner: 'Marvel'
+    },
+    {
+        id: 3,
+        name: 'Superman',
+        owner: 'DC'
+    },
+    {
+        id: 4,
+        name: 'Flash',
+        owner: 'DC'
+    },
+    {
+        id: 5,
+        name: 'Wolverine',
+        owner: 'Marvel'
+    },
+];
+
+export default heroes;
+
+export const owners = ['Marvel', 'DC'];
+/*
+export { 
+    heroes as default,
+    owners
+}*/
+
+/*export default [
+    {
+        id: 1,
+        name: 'Batman',
+        owner: 'DC'
+    },
+    {
+        id: 2,
+        name: 'Spiderman',
+        owner: 'Marvel'
+    },
+    {
+        id: 3,
+        name: 'Superman',
+        owner: 'DC'
+    },
+    {
+        id: 4,
+        name: 'Flash',
+        owner: 'DC'
+    },
+    {
+        id: 5,
+        name: 'Wolverine',
+        owner: 'Marvel'
+    },
+];*/
